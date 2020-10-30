750905877895e8d8b4b532faf6e58dc38cf0ae40
Ian Maurer <ian@genomoncology.com>
2020-10-30 17:28:48 -0400

    Tweaks to Admin UI

---
+++ src/entitykb/http/admin/src/Admin.svelte
@@ -0,0 +1,26 @@
+<table class="ui compact celled definition table">
+  <thead class="full-width">
+    <tr>
+      <th></th>
+      <th>Key</th>
+      <th>Label</th>
+      <th>Name</th>
+      <th>Synonyms</th>
+      <th>Attributes</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td class="collapsing">
+        <div class="ui small primary labeled icon button">
+          <i class="plus icon"></i> Add Node
+        </div>
+      </td>
+      <td>US|COUNTRY</td>
+      <td>COUNTRY</td>
+      <td>United States</td>
+      <td></td>
+      <td></td>
+    </tr>
+  </tbody>
+</table>
--- src/entitykb/http/admin/src/App.svelte
+++ src/entitykb/http/admin/src/App.svelte
@@ -1,11 +1,37 @@
 <script>
-    import Header from "./Header.svelte";
+    import Menu from "./Menu.svelte";
+    import Bottom from "./Bottom.svelte";
+    import Admin from "./Admin.svelte";
+
+    let choice = "admin";
 </script>
 
 <main>
-    <Header/>
+    <Menu bind:choice={choice} />
+
+    {#if (choice === "admin")}
+    <div id="content">
+        <Admin />
+    </div>
+    {:else if (choice === "api")}
+        <iframe src="/docs"></iframe>
+    {:else if (choice === "docs")}
+        <iframe src="https://www.entitykb.org/"></iframe>
+    {:else if (choice === "docs")}
+        <iframe src="https://www.entitykb.org/"></iframe>
+    {/if}
+
+    <Bottom />
 </main>
 
 <style>
-
+    iframe {
+        width: 100%;
+        height: calc(85vh);
+        border: 0;
+    }
+    #content {
+        padding: 1em;
+        margin: 1em;
+    }
 </style>
